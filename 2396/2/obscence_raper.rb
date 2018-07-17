43cdec92b266d502831a3aa4402b492cdd80b3c4
ilya-bitbucket <ilyakulikovitch@yandex.ru>
2018-07-17 18:14:45 +0300

    Add helper and delete text files battles

---
+++ 2396/2/obscence_raper.rb
@@ -0,0 +1,17 @@
+require './battle'
+require './raper'
+
+class ObscenceRaper
+  def self.the_most_obscene_rappers(quantity = nil)
+    obj_rapers = Raper.all
+    quantity ||= obj_rapers.size
+    if obj_rapers.empty?
+      puts 'Object empty or nil'
+    else
+      obj_sort = obj_rapers.sort_by do |obj|
+        1 - obj.size_bad_words
+      end[0..(quantity - 1)]
+      obj_sort.each { |obj| puts obj.show }
+    end
+  end
+end
